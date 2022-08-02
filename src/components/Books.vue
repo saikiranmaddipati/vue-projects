d28cb42543debff85818445e870bddda6cd584fb
saikiranmaddipati <maddipatisaikiran24@gmail.com>
2022-08-02 12:13:47 +0530

    more examples

--- src/components/Books.vue
+++ src/components/Books.vue
@@ -2,6 +2,12 @@
   <div class="Booksworld">
     <h1>{{title}}</h1>
     <p>{{price}}</p>
+    <span v-if="sold">books out of stock</span>
+    <ul>
+      <li v-for="book in books">
+        {{book.title}}
+      </li>
+    </ul>
   </div>
 </template>
 
@@ -11,7 +17,13 @@ export default {
   data (){
     return {
         title:'Word Bulid',
-        price:250
+        price:250,
+        sold: false,
+        books:[
+          {title: 'javascript'},
+          {title: 'python'},
+          {title: 'vuejs'}
+        ]
     }
   }
 }
