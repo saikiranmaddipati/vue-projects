3c31aab4b9782c82c18276463160526caf0982f5
saikiranmaddipati <maddipatisaikiran24@gmail.com>
2022-08-05 11:06:08 +0530

    v-bind and v-model

--- src/components/Books.vue
+++ src/components/Books.vue
@@ -1,13 +1,16 @@
 <template>
   <div class="Booksworld">
+    <h1 v-bind:style="styles">Welcome to the Book World</h1>
     <h1>{{title}}</h1>
     <p>{{price}}</p>
     <span v-if="sold">books out of stock</span>
     <ul>
-      <li v-for="book in books">
+      <li v-for="(book,index) in books" v-bind:key="index">
         {{book.title}}
       </li>
     </ul>
+    <p>{{quantity}}</p>
+    <input type="text" v-model="quantity"/>
   </div>
 </template>
 
@@ -23,8 +26,15 @@ export default {
           {title: 'javascript'},
           {title: 'python'},
           {title: 'vuejs'}
-        ]
+        ],
+        quantity:'5',
+        styles:{
+          backgroundColor:'red',
+          fontSize:'40px'
+        }
     }
   }
 }
 </script>
+
+
